eac2217d5781faa416022859f6742c4303616ac6
TawandaMotsi <128430666+TawandaMotsi@users.noreply.github.com>
2025-04-05 19:03:54 +0100

    .

--- project/pages/About.py
+++ project/pages/About.py
@@ -7,16 +7,13 @@ st.write(
     👋 **Hello!**  
     I’m **Tawanda Motsi** — a technically minded and goal-oriented professional with a unique background in both **Irrigation Engineering** and **Computer Engineering**.
 
-    I hold a Bachelor’s degree in Irrigation Engineering from **Chinhoyi University of Technology** in Zimbabwe and I’m currently completing my BSc in **Computer Engineering** at the **Technological University of the Shannon**. My diverse experience includes designing and installing micro, overhead, and surface irrigation systems, along with pump selection and water system planning.
+    I hold a Bachelor’s degree in Irrigation Engineering from **Chinhoyi University of Technology** in Zimbabwe and I’m currently completing my BSc in **Software Engineering** at the **Technological University of the Shannon**. My diverse experience includes designing and installing micro, overhead, and surface irrigation systems, along with pump selection and water system planning.
 
-    More recently, I’ve been expanding my focus to include **software development**, **data analysis**, and **programming** — with strong proficiency in **Java** and growing experience in building full-stack applications and real-world data science projects.
+    More recently, I’ve been expanding my focus to include **software development**, **data analysis**, and **programming** — with strong proficiency in **Java** and growing experience in building full-stack applications.
 
-    This web application showcases my ability to collect, clean, process, analyze, and visualize data — blending technical skill with practical insight.
+    This web application showcases my ability to collect, clean, process, analyse, and visualise data — blending technical skill with practical insight.
 
-    🧳 After spending 8 months traveling through Central and South America, I’m eager to give back by contributing to organizations that have a meaningful **social impact**.
-
-    If you'd like to connect or have any questions, feel free to reach out via LinkedIn:  
-    🔗 [link]((https://www.linkedin.com/in/tawanda-motsi-24593b84))
+    If you'd like to connect or have any questions, feel free to reach out via LinkedIn:🔗 [link]((https://www.linkedin.com/in/tawanda-motsi-24593b84))
     """,
     unsafe_allow_html=True
 )
